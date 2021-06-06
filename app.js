ead83b95a5abb37d53a463a8d756c3d2ed0e46f0
charity.rogers <rogerscl116@gmail.com>
2021-06-05 22:35:33 -0500

    print dynamic output

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+// notice the lack of parentheses around the `profileDataArr` parameter
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('================');
+
+    // is the same as this...
+    profileDataArr.forEach(profileItem =>
+        console.log(profileItem))       
+};
+
+printProfileData(profileDataArr);
