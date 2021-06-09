7a1111b78b6602078bf15b0ab46a6b9b9a9c3f10
charity.rogers <rogerscl116@gmail.com>
2021-06-09 18:57:37 -0500

    generate entire website

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -132,11 +132,11 @@ promptUser()
     return generatePage(portfolioData);
   })
   .then(pageHTML => {
-    return fs.writeFile(pageHTML);
+    return writeFile(pageHTML);
   })
   .then(writeFileResponse => {
     console.log(writeFileResponse);
-    return fs.copyFile();
+    return copyFile();
   })
   .then(copyFileResponse => {
     console.log(copyFileResponse);
